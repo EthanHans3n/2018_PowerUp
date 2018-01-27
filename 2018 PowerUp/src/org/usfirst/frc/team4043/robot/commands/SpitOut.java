5fc0d3685d2dfa57bd9339733ddc1f44e6c7c565
LaneMikk <35826166+LaneMikk@users.noreply.github.com>
2018-01-27 12:56:38 -0800

    Update SpitOut.java

--- 2018 PowerUp/src/org/usfirst/frc/team4043/robot/commands/SpitOut.java
+++ 2018 PowerUp/src/org/usfirst/frc/team4043/robot/commands/SpitOut.java
@@ -17,7 +17,7 @@ public class SpitOut extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-          subsystems.startYeet();
+          Robot.Intake.startYeet();
     }
 
     // Called repeatedly when this Command is scheduled to run
