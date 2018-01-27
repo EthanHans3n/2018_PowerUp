2d6f10fb4d60c92a571138d5ff5f12059dbe8d67
LaneMikk <35826166+LaneMikk@users.noreply.github.com>
2018-01-27 12:45:12 -0800

    Update Intake.java

--- 2018 PowerUp/src/org/usfirst/frc/team4043/robot/subsystems/Intake.java
+++ 2018 PowerUp/src/org/usfirst/frc/team4043/robot/subsystems/Intake.java
@@ -9,6 +9,20 @@ public class Intake extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    public void startSuck(){
+     RobotMap.intakeFL.set(.5);
+     RobotMap.intakeFR.set(-.5);
+     RobotMap.intakeBL.set(.5);
+     RobotMap.intakeBR.set(-.5);
+    }
+    public void startYeet(){
+     RobotMap.intakeFL.set(-.5);
+     RobotMap.intakeFR.set(.5);
+     RobotMap.intakeBL.set(-.5);
+     RobotMap.intakeBR.set(.5);
+    }
+    
+
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
